6bfa128477d88bce7303955a69623cef4158984d
se0yu <pr924133@gmail.com>
2025-03-05 19:37:50 +0900

    메인 클래스 case문 추가,결과리스트 조회,삭제 기능 개선

--- Calculator.java
+++ Calculator.java
@@ -13,59 +13,43 @@ public class Calculator {
         return new ArrayList<>(resultList);
     }
 
-
+    //결과출력 메서드
     private void printResult() {
         System.out.println("결과 : " + result);
     }
 
-    public void printResultList(String check) {
-        if (check.equals("yes")) {
-            System.out.println(getResultList());
-        }
-    }
-
-    private int sum(int num1, int num2) {
+    //사칙연산 메서드
+    private void sum(int num1, int num2) {
         result = num1 + num2;
-        return result;
     }
 
-    private int sub(int num1, int num2) {
+    private void sub(int num1, int num2) {
         result = num1 - num2;
-        return result;
     }
 
-    private int mul(int num1, int num2) {
+    private void mul(int num1, int num2) {
         result = num1 * num2;
-        return result;
     }
 
-    private int div(int num1, int num2) {
-        if (num2 != 0) {
-            result = num1 / num2;
-        } else {
+    private void div(int num1, int num2) {
+        if (num2 == 0) {
             System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+        } else {
+            result = num1 / num2;
         }
-        return result;
     }
 
     public void calculate(int num1, int num2, char cal) {
         switch (cal) {
-            case '+' -> {
-                sum(num1, num2);
-            }
-            case '-' -> {
-                sub(num1, num2);
-            }
-            case '*' -> {
-                mul(num1, num2);
-            }
-            case '/' -> {
-                div(num1, num2);
-            }
-            default -> {
-                System.out.println("잘못된 입력입니다.");
-                break;
-            }
+            case '+' -> sum(num1, num2);
+
+            case '-' -> sub(num1, num2);
+
+            case '*' -> mul(num1, num2);
+
+            case '/' -> div(num1, num2);
+
+            default -> System.out.println("잘못된 입력입니다.");
         }
         resultList.add(result);
         printResult();
@@ -73,24 +57,32 @@ public class Calculator {
 
 
     //입력받은 check 값에 따라 결과 리스트 삭제&초기화
-    public void checkResultList(String check){
-        if(check.equals("delete")){
-            deleteResult();
-        } else if (check.equals("clear")) {
-            clearResult();
+    public void deleteResultList(int check){
+        switch (check) {
+            case 1 -> removeResult();
+            case 2 -> clearResult();
+            default -> System.out.println("잘못된 숫자를 입력했습니다.");
+            }
         }
+
+    //가장 먼저 추가된 결과 삭제
+    public void removeResult() {
+            if (resultList.isEmpty()) {
+                System.out.println("저장된 결과가 없습니다.");
+            } else {
+                resultList.remove(0);
+                System.out.println("결과 리스트 : "+resultList);
+            }
     }
 
-    public void deleteResult() {
-            if (!resultList.isEmpty()) {
-                resultList.remove(0);
-                System.out.println(resultList);
-            } else {
-                System.out.println("저장된 결과가 없습니다.");
-        }
-    }
+    //결과 리스트 전부 삭제
     public void clearResult() {
+        if(resultList.isEmpty()){
+            System.out.println("이미 결과 리스트가 비어있습니다.");
+        }else {
             resultList.clear();
-            System.out.println(resultList);
+            System.out.println("결과 리스트가 초기화 되었습니다.");
+        }
+
     }
 }
--- Main.java
+++ Main.java
@@ -7,34 +7,52 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         Calculator calculator = new Calculator();
         while (true) {
-            //숫자 입력 받기
-            System.out.print("첫번째 숫자 : ");
-            int num1 = sc.nextInt();
-            System.out.print("두번째 숫자 : ");
-            int num2= sc.nextInt();
-            if (num1 < 0 || num2 < 0) {
-                System.out.println("양의 정수 혹은 0을 입력해주세요.");
-                continue;
-            }
-            //사칙연산 기호 입력 받기
-            System.out.print("사칙연산 기호 입력(+,-,*,/) : ");
-            char cal = sc.next().charAt(0);
+            System.out.println("실행할 기능을 선택하세요.");
+            System.out.println("1. 연산하기 | 2. 결과 리스트 출력하기 | 3. 결과 삭제하기 | 4. 프로그램 종료)");
+            int app = sc.nextInt();
+            switch (app) {
+                case 1 -> {
+                    //숫자 입력 받기
+                    System.out.print("첫번째 숫자 : ");
+                    int num1 = sc.nextInt();
+                    System.out.print("두번째 숫자 : ");
+                    int num2= sc.nextInt();
+                    if (num1 < 0 || num2 < 0) {
+                        System.out.println("양의 정수 혹은 0을 입력해주세요.");
+                        continue;
+                    }
+                    //사칙연산 기호 입력 받기
+                    System.out.print("사칙연산 기호 입력(+,-,*,/) : ");
+                    char cal = sc.next().charAt(0);
 
-            //입력한 값에 따라 사칙연산 출력
-            //calculate 함수 호출(연산 할 숫자와 사칙연산 기호)
-            calculator.calculate(num1,num2,cal);
+                    //입력한 값에 따라 사칙연산 출력
+                    //calculate 함수 호출(연산 할 숫자와 사칙연산 기호)
+                    calculator.calculate(num1,num2,cal);
+                }
+                //결과 리스트 출력
+                case 2 -> {
+                    System.out.println(calculator.getResultList());
+                }
 
-            //결과 리스트 출력
-            System.out.println("저장된 결과 리스트를 출력하시겠습니까?(yes 입력시 출력)");
-            calculator.printResultList(sc.next());
-            //결과 삭제
-            System.out.print("저장된 결과를 삭제 하시겠습니까?(delete 입력시 가장 오래된 결과 삭제,clear 입력시 전부 삭제)");
-            calculator.checkResultList(sc.next());
 
-            //exit 입력시 프로그램 종료.
-            System.out.print("계속하시겠습니까? (exit 입력시 종료)");
-            if (sc.next().equals("exit")) {
-                break;
+                case 3 -> {
+                    //결과 삭제
+                    System.out.print("저장된 결과를 삭제 하시겠습니까?(1.가장 오래된 결과 삭제 | 2.전부 삭제)");
+                    calculator.deleteResultList(sc.nextInt());
+                }
+                case 4 ->{
+                    //exit 입력시 프로그램 종료.
+                    System.out.print("정말 종료하시겠습니까? (exit 입력시 종료)");
+                    if (sc.next().equals("exit")) {
+                        return;
+                    }else{
+                        System.out.println("메뉴로 다시 돌아갑니다.");
+                    }
+                }
+                default -> {
+                    System.out.println("잘못된 숫자를 입력했습니다.");
+                }
+
             }
         }
     }
